6e4d1c18bf718a1fb0363835c4b9fd1b5fcf8382
Munna Ahmed <munnaahmed2025@gmail.com>
2023-06-02 19:29:48 +0600

    commit

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,10 +1,15 @@
+import "./Grid.css";
+
 const Grid = () => {
-  const grid = Array.from({ length: 5 }, (v, i) => i + 1);
+  const grid = Array.from({ length: 12 }, (v, i) => i + 1);
   //   console.log(foo);
   return (
-    <section>
+    <section className="grid gap-2 basic-grid m-2">
       {grid.map((b) => (
-        <div key={crypto.randomUUID()} className="">
+        <div
+          key={crypto.randomUUID()}
+          className="inline-block p-2 bg-slate-500  text-center hover:mx-1 transition-all duration-75"
+        >
           {b}
         </div>
       ))}
